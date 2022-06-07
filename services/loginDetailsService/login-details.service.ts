ead38dc344a60919b6bd4748fd8aef13f95b77e1
vaibhavtekude99 <105300218+vaibhavtekude99@users.noreply.github.com>
2022-06-07 12:21:03 +0530

    Add files via upload

---
+++ services/loginDetailsService/login-details.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import {HttpClient}   from '@angular/common/http';
+import { LoginDetails } from 'src/app/pojo/LoginDetails';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginDetailsService
+ {
+
+  baseURL : string ="http://localhost:8080/employeeDetails/employee";
+  constructor(private http: HttpClient)
+   { 
+
+   }
+
+   
+
+   getAllLoginDetails():Observable<LoginDetails[]>
+   {
+     return this.http.get<LoginDetails[]>(this.baseURL);
+   }
+
+  
+   getSingleLoginDetails(loginId:number):Observable<LoginDetails>
+   {
+     console.log("inside service get Singlelogin method")
+     return this.http.get<LoginDetails>(this.baseURL+'/'+loginId);
+   }
+  
+}
+
+
+
